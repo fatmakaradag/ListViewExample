9baa4735a83abeddcdf662847d9602c8f9864746
U170709061 <fatmakaradag@posta.mu.edu.tr>
2020-10-16 13:17:51 +0300

    Initial commit

---
+++ app/src/main/java/gui/ceng/mu/edu/tr/CustomerAdapterActivity.java
@@ -0,0 +1,28 @@
+package gui.ceng.mu.edu.tr;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CustomerAdapterActivity extends AppCompatActivity {
+    final List<Animal> animals=new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_adapter);
+
+        animals.add(new Animal("Cat",R.drawable.cat));
+        animals.add(new Animal("Lion",R.drawable.lion));
+
+        ListView listView =findViewById(R.id.listview);
+        AnimalAdapter adapter=new AnimalAdapter(animals,this);
+
+
+        listView.setAdapter(adapter);
+    }
+}
---
+++ app/src/main/java/gui/ceng/mu/edu/tr/MainActivity.java
@@ -0,0 +1,38 @@
+package gui.ceng.mu.edu.tr;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Button btnArrayAdapter =findViewById(R.id.button_ArrayAdapter);
+        btnArrayAdapter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent=new Intent(MainActivity.this,ArrayAdapterActivity.class);
+                startActivity(intent);
+            }
+        });
+        Button btnCustomerAdapter=findViewById(R.id.button_CustomAdapter);
+        btnCustomerAdapter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent=new Intent(MainActivity.this,CustomerAdapterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
